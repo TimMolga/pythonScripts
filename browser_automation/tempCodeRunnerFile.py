ee6dcff97ec0228b065c4cad9ac031ba8b104179
TimMolga <49701086+TimMolga@users.noreply.github.com>
2020-05-19 22:56:41 -0400

    Update tempCodeRunnerFile.py

--- browser_automation/tempCodeRunnerFile.py
+++ browser_automation/tempCodeRunnerFile.py
@@ -7,8 +7,8 @@ signin_link = browser.find_element_by_link_text("Sign in")
 signin_link.click()
 
 username_box = browser.find_element_by_id("login_field")
-username_box.send_keys("timothymolga@gmail.com")
+username_box.send_keys("<email>")
 
 password_box = browser.find_element_by_id("password")
-password_box.send_keys("rpg3law0!")
+password_box.send_keys("<password>")
 password_box.submit()
